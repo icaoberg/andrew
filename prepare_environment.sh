04ca8cd9947f07916ec8956130ec0b284da6a60f
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2018-04-17 15:37:17 -0400

    Updated preparation script

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -5,5 +5,15 @@ source ./bin/activate
 
 pip install -I ipython pelican tabulate grid
 pip install -U beautifulsoup4
+pip install pelican-youtube
+pip install pelican-gist
+
+git submodule init
+git submodule update
+
+cd pelican-plugins
+git submodule init
+git submodule update
+cd ..
 
 deactivate
